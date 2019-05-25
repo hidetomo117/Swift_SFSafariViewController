bee7c344cf005035ef82b352f909bba2ab6d34f5
hidetomo117 <hidetomo117@gmail.com>
2019-05-25 15:11:23 +0900

    SafariViewControllerを使ってみた

--- Swift_SFSafariViewController/ViewController/MainViewController/MainViewController.swift
+++ Swift_SFSafariViewController/ViewController/MainViewController/MainViewController.swift
@@ -16,8 +16,57 @@ final class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // MARK: - IBAction
+    
+    @IBAction func tappedSafariButton(_ sender: UIButton) {
+        openSafariViewController()
+    }
 }
 
+// MARK: - Private method
+
 private extension MainViewController {
+
+    private func openSafariViewController () {
+     
+        let googleUrl = NSURL(string: "https://www.google.co.jp/")
+        
+        guard let url = googleUrl, let absoluteURL = url.absoluteURL else {
+            fatalError()
+        }
+        
+        let vc = SFSafariViewController(url: absoluteURL)
+        vc.delegate = self
+        
+        present(vc, animated: false, completion: nil)
+    }
+}
+
+// MARK: SFSafariViewControllerDelegate
+
+extension MainViewController: SFSafariViewControllerDelegate {
     
+    /// ユーザーアクションによりViewControllerがUIActivityViewControllerを表示しようとしている場合に呼び出し(UIActivityをカスタムできる)
+    func safariViewController(_ controller: SFSafariViewController, activityItemsFor URL: URL, title: String?) -> [UIActivity] {
+        return [UIActivity()]
+    }
+    
+    /// ユーザーアクションによりViewControllerがUIActivityViewControllerを表示しようとしている場合に呼び出し(特定のUIActivityTypeを除外できる)
+    func safariViewController(_ controller: SFSafariViewController, excludedActivityTypesFor URL: URL, title: String?) -> [UIActivity.ActivityType] {
+        // 除外したいActivityTypeを配列で返却する(以下の場合はリーディングリストを除外)
+        return [UIActivity.ActivityType.addToReadingList]
+    }
+
+    /// 完了ボタン押下
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+    }
+
+    /// 読み込み完了
+    func safariViewController(_ controller: SFSafariViewController, didCompleteInitialLoad didLoadSuccessfully: Bool) {
+    }
+
+    /// 初期ページのロード中にブラウザが別のURLにリダイレクトされた場合に呼び出し
+    func safariViewController(_ controller: SFSafariViewController, initialLoadDidRedirectTo URL: URL) {
+    }
 }
